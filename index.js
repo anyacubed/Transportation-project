0cc66f2195d16346e4755f49c6854fda55db0027
anyacubed <neverbackdown654@gmail.com>
2021-10-25 20:45:26 +0300

    feat: add arrows

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("here")
+var slider = tns({
+  container: '.salon-images',
+  items: 2,
+  slideBy: 1,
+  nav: false,
+  autoplay: false,
+  controlsPosition: "bottom",
+  controlsText: ["", ""]
+});
