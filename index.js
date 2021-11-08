a27d81c0c7900297a238d9755de317ee1b8ce8bc
anyacubed <neverbackdown654@gmail.com>
2021-11-09 00:08:36 +0300

    feat: add adaptivity

--- index.js
+++ index.js
@@ -27,3 +27,44 @@ scrollUp.onclick = function() {
 window.addEventListener('scroll', function() {
   scrollUp.hidden = (scrollY < document.documentElement.clientHeight);
 });
+
+/* Burger menu */
+
+const burgerMenu = document.querySelector('.burger-menu');
+const burgerMenuButton = document.querySelector('.burger-menu-button');
+const header = document.querySelector('.header');
+const burgerCloseButton = document.querySelector('.burger-close-button');
+const navBurger = document.querySelector('.nav-burger');
+
+burgerMenuButton.addEventListener('click', function(event) { 
+  burgerMenu.classList.toggle('burger-menu-active');
+  header.classList.toggle('header__closed');}
+);
+
+burgerMenuButton.addEventListener('click', function(event) { 
+  burgerCloseButton.classList.toggle('burger-close-button-active');
+  header.classList.toggle('header__closed');}
+);
+
+// burgerMenu.addEventListener('click', () => {
+//   burgerMenu.classList.toggle('active');
+//   navBurger.classList.toggle('active');
+// })
+
+// document.querySelectorAll(".nav-link").forEach(n => n.addEventListener('click', () => {
+//   burgerMenu.classList.remove('active');
+//   navBurger.classList.remove('active');
+// }))
+
+function toggleBurger() {
+  if (burgerMenuButton.style.display == 'block') {
+    burgerMenuButton.style.display ='none';
+    burgerCloseButton.style.display ='block';
+  } else {
+    burgerMenuButton.style.display ='block';
+    burgerCloseButton.style.display ='none';
+  }
+}
+
+burgerMenuButton.addEventListener('click', toggleBurger);
+burgerCloseButton.addEventListener('click', toggleBurger);
